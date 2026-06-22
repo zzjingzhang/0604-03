eba5a5bb0bcdb62f00179e99d5613980d4c60b08
zzjingzhang <1024473568@qq.com>
2026-06-22 12:32:36 +0800

    修改后端无法启动的问题

--- backend/messages/apps.py
+++ backend/messages/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MessagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messages'
+    label = 'user_messages'
+    verbose_name = '消息管理'
